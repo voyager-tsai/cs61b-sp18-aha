0baf28ea442ca92910afdc71ae1e5d18b5d0bd30
aha <freedomjourneyforever@gmail.com>
2024-02-19 22:41:51 +0900

    added lab1 LeapYear.java, first commit for cs61b, haha

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,5 +1,5 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author aha
  */
 public class LeapYear {
 
@@ -27,6 +27,16 @@ public class LeapYear {
             } catch (NumberFormatException e) {
                 System.out.printf("%s is not a valid number.\n", args[i]);
             }
+        }
+    }
+    /** Test whether a year is a leap year or not
+     *  @param  an integer represents a year
+     */
+    public static boolean isLeapYear(int year) {
+        if (((year % 400) == 0) || (((year % 4) == 0) && ((year % 100) != 0))) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
